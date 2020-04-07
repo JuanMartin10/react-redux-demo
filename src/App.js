6ca8063666d6c71f037c7773d34fb08fba871920
Juan Antonio Martin <juanantoniomartinb@gmail.com>
2020-04-07 20:17:31 +0200

    item container done

--- src/App.js
+++ src/App.js
@@ -5,17 +5,23 @@ import CakeContainer from './components/CakeContainer'
 import HooksCakeContainer from './components/HooksCakeContainer'
 import IceCreamContainer from './components/IceCreamContainer'
 import CoffeeContainer from './components/CoffeeContainer'
+import NewCakeContainer from './components/NewCakeContainer'
+import ItemContainer from './components/ItemContainer'
 
 import './App.css';
+
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake />
+        <ItemContainer />
         <HooksCakeContainer />
         <CakeContainer />
         <IceCreamContainer />
         <CoffeeContainer />
+        <NewCakeContainer />
       </div>
     </Provider>
   );
